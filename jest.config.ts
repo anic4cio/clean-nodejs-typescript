7fdd1f4d4c7ee89b992832a70d3abb4f671265af
anic4cio <anicaciovinicius36@gmail.com>
2023-07-25 19:33:40 -0300

    refactor: jest config file

--- jest.config.ts
+++ jest.config.ts
@@ -3,5 +3,7 @@ export default {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
-  transform: { '.+\\.ts': 'ts-jest' }
-};
+  transform: {
+    '.+\\.ts': 'ts-jest'
+  }
+}
